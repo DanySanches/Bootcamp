a42846b0ff457296e7540ae82a98ef7197584e97
danysanches@msn.com <danysanches@msn.com>
2024-09-03 19:36:07 -0300

    revisão  variáveis

---
+++ script.js
@@ -0,0 +1,48 @@
+//var , let e const
+
+var nome = 'Dani';
+
+
+console.log(nome);
+
+nome = 'Mel'
+
+function saudacao(){
+    var mensagem = 'olá';
+    console.log(mensagem);
+}
+
+if(true){
+   var nome = 'Pipoca'
+}
+
+console.log(nome);
+//console.log(saudacao);
+saudacao()
+
+
+//let
+let gato = "Tigrinho";
+console.log(gato);
+
+if(true){
+    let gato2 ="Nino";
+    console.log(gato2)
+}
+
+
+// const
+const  msg = "olá";
+console.log(msg);
+
+const cont = {
+    nome: "Dani",
+    idade: "37",
+}
+
+cont.nome - 'Nina';
+
+
+
+
+
